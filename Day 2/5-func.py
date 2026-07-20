ee3e6035438bfb7e288cb61813bb90a9b0a326e8
Gaurav Joshi <gauravrpjoshi1205@gmail.com>
2026-07-20 16:15:30 +0530

    Modified 5th file.

--- Day 2/5-func.py
+++ Day 2/5-func.py
@@ -15,3 +15,15 @@ def area_shapes (shape, *args):
         return area
     else:
         return "Invalid shape"
+shape=input("Enter shape (circle, rectangle, triangle): ")
+if shape == "circle":
+    radius = float(input("Enter radius: "))
+    print("Area of circle:", area_shapes(shape, radius))
+elif shape == "rectangle":
+    length = float(input("Enter length: "))
+    breadth = float(input("Enter breadth: "))
+    print("Area of rectangle:", area_shapes(shape, length, breadth))
+elif shape == "triangle":
+    base = float(input("Enter base: "))
+    height = float(input("Enter height: "))
+    print("Area of triangle:", area_shapes(shape, base, height))
